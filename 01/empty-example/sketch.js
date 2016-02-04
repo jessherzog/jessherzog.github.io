a928be439c08d2f84af160dbdf240cc7e30a97e7
Jess <jsscherzog@yahoo.com>
2016-02-04 00:49:33 -0500

    HW 1 : fixed sketch in index.html

--- 01/empty-example/sketch.js
+++ 01/empty-example/sketch.js
@@ -11,8 +11,9 @@ function setup() {
 }
 
 function draw() {
+  noStroke();
+  fill(158, 212, 144);
   rect(0, 320, width, 200);
-  image(NW, 100, 300, NW.width/2, NW.height/2);
-  imageMode(CENTER);
+  image(NW, 100, 200, NW.width/2, NW.height/2);
   image(SE, random(300), random(300), random(SE.width), random(SE.height));
 }
