d8924d0cb00b1a0b0d18af4f9a60373e8886dfa4
Jess <jsscherzog@yahoo.com>
2016-02-03 22:11:40 -0500

    added new Homework 1 sketch with images

--- 01/empty-example/sketch.js
+++ 01/empty-example/sketch.js
@@ -1,16 +1,18 @@
-// Learning Processing
-// Daniel Shiffman
-// http://www.learningprocessing.com
+var NW;
+var SE;
 
-// Example 1-1: stroke and fill
+function preload() {
+  NW = loadImage("assets/dude.gif");
+  SE = loadImage("assets/fleur.gif");
+}
 
 function setup() {
-  createCanvas(480, 270);
-  stroke(0); 
-  fill(150);
+  createCanvas(300, 400);
 }
 
 function draw() {
-  background(255);
-  rect(50,50,75,100);
+  rect(0, 320, width, 200);
+  image(NW, 100, 300, NW.width/2, NW.height/2);
+  imageMode(CENTER);
+  image(SE, random(300), random(300), random(SE.width), random(SE.height));
 }
