19f637aeed9616dd9f287f778166afb8a967305f
Jess <jsscherzog@yahoo.com>
2016-09-18 23:22:09 -0400

    fixed gpx filepaths

--- 1-example/sketch.js
+++ 1-example/sketch.js
@@ -8,31 +8,31 @@ L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/
     accessToken: 'pk.eyJ1IjoieGl1eGl1eHh4IiwiYSI6ImNpdDFuM3VxYjBxem4ycHFwcHAzMG5lZHIifQ.dvASaKLVsXfV_2tN2aATMg'
 }).addTo(mymap);
 
-var run93Layer = omnivore.gpx('/assets/routes GPX/sept_3.gpx')
+var run93Layer = omnivore.gpx('assets/routes GPX/sept_3.gpx')
     .on('ready', function() {
         mymap.fitBounds(run93Layer.getBounds());
     })
     .addTo(mymap);
 
-var run94Layer = omnivore.gpx('/assets/routes GPX/sept_4.gpx')
+var run94Layer = omnivore.gpx('assets/routes GPX/sept_4.gpx')
 .on('ready', function() {
     mymap.fitBounds(run94Layer.getBounds());
 })
 .addTo(mymap);
 
-var run95Layer = omnivore.gpx('/assets/routes GPX/sept_5.gpx')
+var run95Layer = omnivore.gpx('assets/routes GPX/sept_5.gpx')
 .on('ready', function() {
     mymap.fitBounds(run95Layer.getBounds());
 })
 .addTo(mymap);
 
-var run96Layer = omnivore.gpx('/assets/routes GPX/sept_6.gpx')
+var run96Layer = omnivore.gpx('assets/routes GPX/sept_6.gpx')
 .on('ready', function() {
     mymap.fitBounds(run96Layer.getBounds());
 })
 .addTo(mymap);
 
-var run97Layer = omnivore.gpx('/assets/routes GPX/sept_7.gpx')
+var run97Layer = omnivore.gpx('assets/routes GPX/sept_7.gpx')
     .on('ready', function() {
         mymap.fitBounds(run97Layer.getBounds());
     })
